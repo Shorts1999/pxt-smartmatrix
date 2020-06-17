de3edf93633beb8c53aeaecb3b5096c0a9f4769b
Shorts1999 <53516987+Shorts1999@users.noreply.github.com>
2020-06-17 15:14:15 +0200

    Update pxt.json, main.ts, test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,4 @@
 // tests go here; this will not be compiled when this package is used as a library
+let test=SmartMatrix.create(DigitalPin.P0, 0, 0, NeoPixelMode.RGB)
+
+test.scrollText("H", 10, 0xff0000)
